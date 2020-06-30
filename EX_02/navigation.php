1908d88b6a1579a74d78c2662c2c36ffbc2cbcbb
jadehontanx <67281421+jadehontanx@users.noreply.github.com>
2020-06-30 10:04:07 +0200

    j05

---
+++ EX_02/navigation.php
@@ -0,0 +1,5 @@
+<nav>
+        <a href="vitrine-accueil.php">Accueil</a>
+        <a href="vitrine-programme.php">Programme</a>
+        <a href="vitrine-contacts.php">Contact</a>  
+</nav>
---
+++ EX_02/vitrine-contacts.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/vitrine.css">
+</head>
+    <body>
+        <?php require "header.php"?>
+<h2>Contact</h2>
+<fieldset>
+    <fieldset>
+        <label for="nom et prénom">Votre nom</label>
+        <input size="20" maxlength="40" />
+    </fieldset>
+    <fieldset>
+        <label for="email">Votre email :</label>
+        <input size="20" maxlength="40" />
+    </fieldset>
+    <fieldset>
+        <label for="message">Comment améliorer mon site?</label>
+        <input size="20" maxlength="40" />
+    </fieldset>
+    <p>
+        <input type="submit" value="Envoyer" />
+</body>
+<?php require "footer.php"?>
+</html>
